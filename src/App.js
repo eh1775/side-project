048e276f4583d704559ce12649e2413ace3df352
Emily Hancock <emilyhancock@Emilys-MacBook-Air-2.local>
2019-04-23 15:55:50 +0800

    side project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ResultComponent from './ResultComponent.js';
+import KeyPadComponent from "./KeyPadComponent.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: ""
+        }
+    }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        //let checkResult = ''
+        //checkResult = this.state.result
+
+        try {
+            this.setState({
+                result: "Sorry. Your Numbers Don't Match. You lose 10 points. Play Again to win them back!",
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    render() {
+        return (
+            <div className= "App">
+                <div className="calculator-body">
+                    <h1>Pick 3 Lottery</h1>
+                    <h3>Select 3 Numbers from 0 to 9</h3> 
+                    <h3>If your numbers match the house, you win 100 Points!</h3>
+                    <KeyPadComponent onClick={this.onClick}/>
+                    <ResultComponent result={this.state.result}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
